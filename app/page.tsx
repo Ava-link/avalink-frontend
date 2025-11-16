cf75de066d0c8bd12dd57e33c607513101027287
shivraj-kore <shivrajkore100@gmail.com>
2025-11-16 19:52:02 +0530

    minor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -78,7 +78,6 @@ export default function Landing() {
         >
           <h2 className="font-bold mb-3">FEATURES<br/></h2>
           -- MULTI-CHAIN SUPPORT<br/>
-          -- LOW GAS FEES<br/>
           -- HIGH THROUGHPUT<br/>
           -- INSTANT BRIDGING<br/>
           -- INSTANT DEPLOYMENT<br/>
@@ -95,7 +94,7 @@ export default function Landing() {
             darkMode ? "border-gray-700" : "border-black"
           )}
         >
-          <h2 className="font-bold mb-3">EXOSYSTEMS</h2>
+          <h2 className="font-bold mb-3">ECOSYSTEMS</h2>
           -- Ecosystems is a next-generation interoperability protocol layer built for the Avalanche, enabling seamless and scalable token movement between Avalanche L1s<br/>
           <div className="text-red-500">
             ●●●●●●●●<br/><br/>
